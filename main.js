41df76a24f5947d507655a9cad0ccbacc7555904
Matthew Sturtevant <sturtevantma21@gmail.com>
2024-10-15 20:57:07 -0400

    Basic Boiler Plate Stuff (NOT COMPLETE)

---
+++ main.js
@@ -0,0 +1,30 @@
+//main.js
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 400,
+        height: 300,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    mainWindow.loadFile('index.html');
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) createWindow();
+});
